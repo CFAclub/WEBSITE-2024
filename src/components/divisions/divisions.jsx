3bc6e7aeb4892da17309178e4a6f387dd801ec40
Pratham <cse220001060@iiti.ac.in>
2024-05-28 12:12:01 +0530

    added division photos

--- src/components/divisions/divisions.jsx
+++ src/components/divisions/divisions.jsx
@@ -7,16 +7,22 @@ const Divisions = () => {
             <h1 className={styles.heading}>DIVISIONS</h1>
             <div className={styles.imagesWrapper}>
                 <div className={styles.imageContainer}>
-                    <img src="https://th.bing.com/th/id/R.ef6b1c102f8c9d91c6d079844f701255?rik=5GsF9LI424HfFA&riu=http%3a%2f%2feskipaper.com%2fimages%2flandscape-wallpaper-hd-10.jpg&ehk=519130nVNM1Nik8Sj1VriQGc%2bTzk5oB18rji3yocypQ%3d&risl=&pid=ImgRaw&r=0" alt="Image 1" className={styles.ellipticalImage} />
-                    <p className={styles.imageTitle}>CONSULTING AND PRODUCT MANAGEMENT</p>
+                    <a href='/consulting'>
+                        <img src="/Consulting.jpg" alt="Image 1" className={styles.ellipticalImage} />
+                        <p className={styles.imageTitle}>CONSULTING AND PRODUCT MANAGEMENT</p>
+                    </a>
                 </div>
                 <div className={styles.imageContainer}>
-                    <img src="https://th.bing.com/th/id/R.ef6b1c102f8c9d91c6d079844f701255?rik=5GsF9LI424HfFA&riu=http%3a%2f%2feskipaper.com%2fimages%2flandscape-wallpaper-hd-10.jpg&ehk=519130nVNM1Nik8Sj1VriQGc%2bTzk5oB18rji3yocypQ%3d&risl=&pid=ImgRaw&r=0" alt="Image 2" className={styles.ellipticalImage} />
-                    <p className={styles.imageTitle}>FINANCE AND QUANT</p>
+                    <a href="/finance">
+                        <img src="Finance.jpg" alt="Image 2" className={styles.ellipticalImage} />
+                        <p className={styles.imageTitle}>FINANCE AND QUANT</p>
+                    </a>
                 </div>
                 <div className={styles.imageContainer}>
-                    <img src="https://th.bing.com/th/id/R.ef6b1c102f8c9d91c6d079844f701255?rik=5GsF9LI424HfFA&riu=http%3a%2f%2feskipaper.com%2fimages%2flandscape-wallpaper-hd-10.jpg&ehk=519130nVNM1Nik8Sj1VriQGc%2bTzk5oB18rji3yocypQ%3d&risl=&pid=ImgRaw&r=0" alt="Image 3" className={styles.ellipticalImage} />
-                    <p className={styles.imageTitle}>DATA SCIENCE AND ANALYTICS</p>
+                    <a href="/analytics">
+                        <img src="Analytics.jpg" alt="Image 3" className={styles.ellipticalImage} />
+                        <p className={styles.imageTitle}>DATA SCIENCE AND ANALYTICS</p>
+                    </a>
                 </div>
             </div>
         </div>
