33f31308819f8bc0f5d5acfdc2dd7b2486779b5c
Dikshit Suthar <suthardikshit57@gmail.com>
2024-06-12 21:32:09 +0530

    mailing section designed

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,46 +1,72 @@
-import React, { useState } from 'react'
-import '../../styles/subscribe.css'
+import React, { useState } from "react";
+import "../../styles/subscribe.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 // import { doc, updateDoc, arrayUnion } from "firebase/firestore";
 // import { db } from '../../utils/firebase';
 
 export default function Subscribe() {
+  // const [email, setEmail] = useState("");
+  // const subscribe = async () => {
+  //     try {
+  //         const emailListRef = doc(db, 'emails', "emailList");
 
-    // const [email, setEmail] = useState("");
-    // const subscribe = async () => {
-    //     try {
-    //         const emailListRef = doc(db, 'emails', "emailList");
+  //         await updateDoc(emailListRef, {
+  //             emails: arrayUnion(email)
+  //         });
+  //     } catch (e) {
+  //         console.error("Error adding document: ", e);
+  //     }
+  // }
 
-    //         await updateDoc(emailListRef, {
-    //             emails: arrayUnion(email)
-    //         });
-    //     } catch (e) {
-    //         console.error("Error adding document: ", e);
-    //     }
-    // }
-
-    return (
-        <>
-        <div>
-            {/* <input type='text' onChange={(e) => { setEmail(e.target.value) }} />
+  return (
+    <>
+      <div>
+        {/* <input type='text' onChange={(e) => { setEmail(e.target.value) }} />
             <button onClick={() => { subscribe() }}>Subscribe</button>
             <p>Are you looking for collaboration? Send an email </p>
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=someone@gmail.com&su=Collaboration">Send an email</a> */}
-        </div>
+      </div>
 
-        <div className="subcontainer">
-            <div className="leftcontainer">
-                <div className="leftcontainer1">
-                <h1 id='heading1'>SUBSCRIBE TO OUR MAILING LIST</h1>
-                <div className="lowercontainer flex">
-                <input type="email" className='sub-input' placeholder='Enter your email here'/>
-               <button className='btnsubscribe' type='submit'>Subscribe</button>
-                </div>
-                <span>To see your mailing preferences,click here.If you are using a work email,you might need to whitelist cfa@iiti.ac.in to receive your emails.</span>
-                </div>
+      <div className="subcontainer">
+        <div className="leftcontainer">
+          <div className="leftcontainer1">
+            <h1 id="heading1">SUBSCRIBE TO OUR MAILING LIST</h1>
+            <div className="lowercontainer flex">
+              <input
+                type="email"
+                className="sub-input"
+                placeholder="Enter your email here"
+              />
+              <button className="btnsubscribe" type="submit">
+                Subscribe
+              </button>
             </div>
-            <div className="rightcontainer"></div>
+            <span>
+              To see your mailing preferences,click here.If you are using a work
+              email,you might need to whitelist cfa@iiti.ac.in to receive your
+              emails.
+            </span>
+          </div>
         </div>
-
-        </>
-    )
+        <div className="rightcontainer">
+          <div id="rightcontainer1">
+            <div className="font-medium text-start text-white p-3">
+              Are you looking forward to a collaboration? Send an email.
+            </div>
+           <span id="mailtext" > 
+            <span >
+              <a href="mailto:cfa@iiti.ac.in">
+                <FontAwesomeIcon icon={faEnvelope} />
+              <span> | Send an email</span>
+            
+             
+            </a>
+            </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
