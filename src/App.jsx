b3cb8a0f41f7565fed6381320d5a047ec4a68908
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-10-16 16:46:56 +0530

    layout fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import React ,  { useEffect, useState } from 'react'
-import Page from './page/page'
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Page from "./page/page";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
 
 function App() {
-  const[isloading, setIsLoading] = useState(true)
+  const [isloading, setIsLoading] = useState(true);
   useEffect(() => {
-    setIsLoading(false)
-  }, [])
+    setIsLoading(false);
+  }, []);
 
-  if(isloading) return null;
+  if (isloading) return null;
   else
-  return (
-    <>
-      <Page/>
-    </>
-  )
+    return (
+      <div className=" max-w-[100dvw] overflow-x-clip">
+        <Page />
+      </div>
+    );
 }
 
-export default App
+export default App;
